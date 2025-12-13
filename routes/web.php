f3dbdc4993864c7bcd835ee9c8ab09643fa32b4f
Ivan Gerasimov <pqlilpd@gmail.com>
2025-12-13 18:22:27 +0300

    updated

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,15 +24,19 @@ Route::get('about', [PageController::class, 'about'])->name('about');
 //     return view('about');
 // })->name('about');
 
-Route::get('articles', function () {
-    $test = App\Models\Article::test32();
-    $articles = App\Models\Article::all();
-    $first = App\Models\Article::first();
-    $two = App\Models\Article::find(2);
-    return view('articles', [
-        'test' => $test,
-        'two' => $two,
-        'first' => $first,
-        'articles' => $articles
-    ]);
-});
+// Название сущности в URL во множественном числе, контроллер в единственном
+Route::get('articles', [ArticleController::class, 'index'])
+  ->name('articles.index'); // имя маршрута, нужно для того, чтобы не создавать ссылки руками
+
+// Route::get('articles', function () {
+//     $test = App\Models\Article::test32();
+//     $articles = App\Models\Article::all();
+//     $first = App\Models\Article::first();
+//     $two = App\Models\Article::find(2);
+//     return view('articles', [
+//         'test' => $test,
+//         'two' => $two,
+//         'first' => $first,
+//         'articles' => $articles
+//     ]);
+// });
